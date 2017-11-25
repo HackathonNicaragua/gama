f54c08824ea2c2cef9e803b908bddc5b0cf64426
adal6a <adalgarcia17@gmail.com>
2017-11-25 17:34:31 -0600

    users agregados a la app para el login

--- medical/database/seeds/DatabaseSeeder.php
+++ medical/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $users = [[
+            'name' => "adal",
+            'nombres' => "Adal",
+            'apellidos' => "Garcia",
+            'cedula' => "2411904960005s",
+            'fecha_registro' => "2017-11-01 00:00:00",
+            'email' => "adal@gmail.com",
+            'password' => bcrypt('contra'),
+            'tipo' => 0,
+        ],
+
+        [
+            'name' => "alvaro",
+            'nombres' => "Alvaro",
+            'apellidos' => "Chavez",
+            'cedula' => "2411904960005s",
+            'fecha_registro' => "2017-11-01 00:00:00",
+            'email' => "alvaro@gmail.com",
+            'password' => bcrypt('contra'),
+            'tipo' => 0,
+        ],
+
+        [
+            'name' => "mason",
+            'nombres' => "Mason",
+            'apellidos' => "Urbina",
+            'cedula' => "2411904960005s",
+            'fecha_registro' => "2017-11-01 00:00:00",
+            'email' => "mason@gmail.com",
+            'password' => bcrypt('contra'),
+            'tipo' => 0,
+        ],
+
+        [
+            'name' => "gerald",
+            'nombres' => "Gerald",
+            'apellidos' => "Sandoval",
+            'cedula' => "2411904960005s",
+            'fecha_registro' => "2017-11-01 00:00:00",
+            'email' => "gerald@gmail.com",
+            'password' => bcrypt('contra'),
+            'tipo' => 0,
+        ]
+
+    	];
+
+    	User::insert($users);
     }
 }
