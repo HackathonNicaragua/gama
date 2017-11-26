6a1d08e6de380bf81f0f76d4c3dc6b9563e4418b
MasonUGutierrez <msn.guti5395@gmail.com>
2017-11-26 11:32:21 -0600

    Cambiando login fondo

--- medical/resources/views/vendor/adminlte/auth/login2.blade.php
+++ medical/resources/views/vendor/adminlte/auth/login2.blade.php
@@ -5,63 +5,74 @@
 @endsection
 
 @section('content')
-    <body class="hold-transition login-page">
-    <div id="app" v-cloak>
-        <div class="login-box">
-            <div class="login-logo">
-                <a href="{{ url('/home') }}"><b>Medi-System</b></a>
-            </div><!-- /.login-logo -->
+    <body class="hold-transition login-page no-padding no-margin">
+    <div class="banner">
+        <div class="bg-color">
+            <div class="flex-center position-ref full-height">
+                <div id="app" v-cloak>
+                    <div class="login-box">
+                        <div class="login-logo">                
+                            <a href="{{ url('/home') }}"><b>Medi-System</b></a>
+                            <!-- <div>
+                                <img src="/img/LogoOficial.png" class="img-responsive" style="width: 250px; height: 250px;margin: auto;" alt="logo">
+                            </div> -->
+                        </div><!-- /.login-logo -->
 
-            @if (count($errors) > 0)
-                <div class="alert alert-danger">
-                    <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
-            <div class="login-box-body">
-                <p class="login-box-msg"> {{ trans('adminlte_lang::message.siginsession') }} </p>
-                <form action="{{ url('/login') }}" method="post">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <login-input-field
-                            name="{{ config('auth.providers.users.field','email') }}"
-                            domain="{{ config('auth.defaults.domain','') }}"
-                    ></login-input-field>
-                    <div class="form-group has-feedback">
-                    <input class="form-control" placeholder="Usuario o correo electrónico" name="email"/>
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                    </div>
-                    <div class="form-group has-feedback">
-                        <input type="password" class="form-control" placeholder="{{ trans('adminlte_lang::message.password') }}" name="password"/>
-                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-8">
-                            <div class="checkbox icheck">
-                                <label>
-                                    <input style="display:none;" type="checkbox" name="remember"> {{ trans('adminlte_lang::message.remember') }}
-                                </label>
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
                             </div>
-                        </div><!-- /.col -->
-                        <div class="col-xs-12">
-                            <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.buttonsign') }}</button>
-                        </div><!-- /.col -->
-                    </div>
-                </form>
+                        @endif
 
-                {{-- @include('adminlte::auth.partials.social_login') --}}
+                        <div class="login-box-body">
+                            <p class="login-box-msg"> {{ trans('adminlte_lang::message.siginsession') }} </p>
+                            <form action="{{ url('/login') }}" method="post">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <login-input-field
+                                        name="{{ config('auth.providers.users.field','email') }}"
+                                        domain="{{ config('auth.defaults.domain','') }}"
+                                ></login-input-field>
+                                <div class="form-group has-feedback">
+                                <input class="form-control" placeholder="Usuario o correo electrónico" name="email"/>
+                                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                                </div>
+                                <div class="form-group has-feedback">
+                                    <input type="password" class="form-control" placeholder="{{ trans('adminlte_lang::message.password') }}" name="password"/>
+                                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-8">
+                                        <div class="checkbox icheck">
+                                            <label>
+                                                <input style="display:none;" type="checkbox" name="remember"> {{ trans('adminlte_lang::message.remember') }}
+                                            </label>
+                                        </div>
+                                    </div><!-- /.col -->
+                                    <div class="col-xs-12">
+                                        <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.buttonsign') }}</button>
+                                    </div><!-- /.col -->
+                                </div>
+                            </form>
 
-                {{-- <a href="{{ url('/password/reset') }}">{{ trans('adminlte_lang::message.forgotpassword') }}</a><br> --}}
-                {{-- <a href="{{ url('/register') }}" class="text-center">{{ trans('adminlte_lang::message.registermember') }}</a> --}}
+                            {{-- @include('adminlte::auth.partials.social_login') --}}
 
-            </div><!-- /.login-box-body -->
+                            {{-- <a href="{{ url('/password/reset') }}">{{ trans('adminlte_lang::message.forgotpassword') }}</a><br> --}}
+                            {{-- <a href="{{ url('/register') }}" class="text-center">{{ trans('adminlte_lang::message.registermember') }}</a> --}}
 
-        </div><!-- /.login-box -->
+                        </div><!-- /.login-box-body -->
+
+                    </div><!-- /.login-box -->
+                </div>
+            </div>  
+        </div>
     </div>
+    
+    
     @include('adminlte::layouts.partials.scripts_auth')
 
     <script>
