2cb67e1b072704523f6ba0bac6bb2b9db2648e17
adal6a <adalgarcia17@gmail.com>
2017-11-25 18:52:37 -0600

    plantilla base limpia

--- medical/routes/web.php
+++ medical/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('adminlte::auth.login2');
 });
 
 Route::group(['middleware' => 'auth'], function () {
@@ -25,3 +25,5 @@ Route::group(['middleware' => 'auth'], function () {
 });
 
 Route::auth();
+
+
