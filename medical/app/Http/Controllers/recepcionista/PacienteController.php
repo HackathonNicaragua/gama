85618c1785d60dd5a3daff77c35f93a8adabf329
Gerald <geraldfreax@gmail.com>
2017-11-26 00:07:55 -0600

    Completado registro de pacientes

--- medical/app/Http/Controllers/recepcionista/PacienteController.php
+++ medical/app/Http/Controllers/recepcionista/PacienteController.php
@@ -23,31 +23,31 @@ class PacienteController extends Controller
 
     public function create()
     {
+        $forms = request()->except('_token');
 
-        if(request()->input("record_id")==null)
-        {
-            $p = paciente::save(request()->all());
-        }else{
-            $p = paciente::find(request()->input("record_id"));
+        if(request()->input('id_paciente')!=null)
+          $p = paciente::find(request()->input('id_paciente'));
+        else
+          $p = new paciente;
 
-            if(request()->input("nombres")!=null)
-              $p->nombres = request()->input("nombres");
+        if(request()->input('nombres')!=null)
+          $p->nombres = request()->input('nombres');
 
-            if(request()->input("apellidos")!=null)
-              $p->apellidos = request()->input("apellidos");
+        if(request()->input('apellidos')!=null)
+          $p->apellidos = request()->input('apellidos');
 
-            if(request()->input("cedula")!=null)
-              $p->cedula = request()->input("cedula");
+        if(request()->input('cedula')!=null)
+          $p->cedula = request()->input('cedula');
 
-            if(request()->input("edad")!=null)
-              $p->edad = request()->input("edad");
+        if(request()->input('edad')!=null)
+          $p->edad = request()->input('edad');
 
-            if(request()->input("sexo")!=null)
-              $p->sexo = request()->input("sexo");
+        if(request()->input('sexo')!=null)
+          $p->sexo = request()->input('sexo');
 
-        }
+        $p->save();
 
+        return $this->createForm($p['id_paciente']);
 
-        return dd(request());
     }
 }
